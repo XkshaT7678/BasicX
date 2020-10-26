6cee1f016afbc416a3ce0dc2c5bdb6136d0db171
MrHacker-X <techxyz09@gmail.com>
2020-10-26 21:09:29 +0530

    version1.0

---
+++ README.md
@@ -0,0 +1,1 @@
+This tool is for tetmux beginners user ,This tool will automatically install all basic required packages so clone it in termux and run it
---
+++ install.sh
@@ -0,0 +1,87 @@
+# All requirements basic packages installation in termux
+pkg=apt
+
+echo  "     ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗       ██╗  ██╗"
+echo  "     ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗      ╚██╗██╔╝"
+echo  "     ███████║███████║██║     █████╔╝ █████╗  ██████╔╝█████╗ ╚███╔╝"
+echo  "     ██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗╚════╝ ██╔██╗"
+echo  "     ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║      ██╔╝ ██╗"
+echo  "     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝      ╚═╝  ╚═╝"
+
+echo  ""
+
+echo "----------------------------------------------------------------------------- "
+echo " Created By HackerX "
+echo " Youtube :> https://www.youtube.com/Hacker X/ "
+echo "---------------------------------------------------------------------------------"
+
+echo "press ENTER to continue"
+read
+
+pkg install w3m                   #update in progress                  #installation......
+pkg install python
+pkg install python2
+pkg install ruby
+pkg install git
+pkg install php
+pkg install perl
+pkg install nmap
+pkg install termux-tools
+pkg install termux-api
+pkg install termux-licenses
+pkg install util-linux
+pkg install science-repo
+pkg install o-editor
+pkg install neofetch
+pkg install netcat
+pkg install linux-man-pages
+pkg install libzip
+pkg install libssh
+pkg install libssh2
+pkg install hydra
+pkg install game-repo
+pkg install fish
+pkg install dpkg
+figlet 50%
+pkg install dash
+pkg install autossh
+pkg install apt
+pkg install abook
+pkg install zlib
+pkg install zsh
+pkg install z3
+pkg install play-audio
+pkg install pkg-config
+pkg install bash
+pkg install clang
+pkg install jq
+pkg install macchanger
+pkg install nano
+pkg install curl
+pkg install tar
+pkg install zip
+pkg install unzip
+pkg install tor
+pkg install wget
+pkg install wcalc
+pkg install openssl
+pkg install bmon
+pkg install openssh
+clear
+echo  " "
+echo  " "
+
+
+
+echo  "     ██████╗  ██████╗ ███╗   ██╗███████╗"
+echo  "     ██╔══██╗██╔═══██╗████╗  ██║██╔════╝"
+echo  "     ██║  ██║██║   ██║██╔██╗ ██║█████╗"
+echo  "     ██║  ██║██║   ██║██║╚██╗██║██╔══╝"
+echo  "     ██████╔╝╚██████╔╝██║ ╚████║███████╗"
+echo  "     ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝"
+echo  " "
+
+
+echo  " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
