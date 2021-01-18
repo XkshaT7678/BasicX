5ef9548a7d399c439a04dc39bb2ebbd2046b0cf9
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2021-01-19 01:03:19 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,115 @@
+# All requirements basic packages installation in termux
+clear
+cd
+echo "\033[1;92m██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗       ██╗  ██╗
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗      ╚██╗██╔╝
+███████║██║  ██║██║     █████╔╝ █████╗  ██████╔╝█████╗ ╚███╔╝
+██╔══██║██║  ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗╚════╝ ██╔██╗
+██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║      ██╔╝ ██╗
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝      ╚═╝  ╚═╝"
+echo "---------------------------------------------------------------"
+echo "| Created By MrHacker-X                                       |"
+echo "| Youtube :> https://www.youtube.com/MrHacker X/              |"
+echo "---------------------------------------------------------------"
+termux-open-url https://youtu.be/m_9uPmRLQnI
+echo "press ENTER to continue"
+read g
+pkg update && pkg upgrade
+apt install w3m -y
+apt install python -y
+apt install python2 -y
+pip install lolcat
+apt install figlet -y
+apt install ruby -y
+apt install git -y
+apt install php -y
+apt install perl -y
+apt install nmap -y
+apt install termux-tools -y
+apt install termux-api -y
+apt install termux-licenses -y
+apt install util-linux -y
+apt install science-repo -y
+apt install o-editor -y
+figlet 25% Completed | lolcat
+sleep 1.6
+apt install neofetch -y
+apt install netcat -y
+apt install linux-man-pages -y
+apt install libzip -y
+apt install libssh -y
+apt install libssh2 -y
+apt install hydra -y
+apt install game-repo -y
+apt install fish -y
+apt install dapt -y
+apt install dash -y
+apt install autossh -y
+apt install cmatrix -y
+apt install abook -y
+apt install zlib -y
+figlet 50% Completed | lolcat
+sleep 1.6
+apt install zsh -y
+apt install z3 -y
+apt install play-audio -y
+apt install apt-config -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y
+apt install openssl -y
+apt install bmon -y
+figlet 75% Completed | lolcat
+sleep 1.6
+apt install openssh -y
+apt install mpv -y
+apt install jp2a -y
+clear
+figlet pip packages installing | lolcat
+echo ''
+echo ''
+pip install requests
+pip2 install requests
+pip install beautifulsoup4
+pip install bs4
+pip install certifi
+pip install chardet
+pip install colorama
+pip install idna
+pip install PySocks
+pip install setuptools
+pip install soupsieve
+pip install stem
+pip install urllib3
+figlet 100% Completed | lolcat
+sleep 1.6
+clear
+echo ""
+echo ""
+echo ""
+echo "     ██████╗  ██████╗ ███╗   ██╗███████╗
+     ██╔══██╗██╔═══██╗████╗  ██║██╔════╝
+     ██║  ██║██║   ██║██╔██╗ ██║█████╗
+     ██║  ██║██║   ██║██║╚██╗██║██╔══╝
+     ██████╔╝╚██████╔╝██║ ╚████║███████╗
+     ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+All termux basic packages installed in your termux.
+Thanks for using my tool for packages installation
+______________________________________
+      Follow us on social media
+    _______________________________
+    | Instagram: hackerx30        |
+    | YouTube: MrHacker X         |
+    | Github: MrHacker-X          |
+    -------------------------------
+ " | lolcat
+
+termux-open-url https://youtube.com/channel/UC2t1smKARnlzoqELbyEhXVw
